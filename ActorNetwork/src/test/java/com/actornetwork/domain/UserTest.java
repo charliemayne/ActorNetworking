60993237b0eeecc6ac1d8e7fe3371335f8dcc930
Wolfgang-Zoppelt <89922472+Wolfgang-Zoppelt@users.noreply.github.com>
2024-10-12 18:28:48 -0400

    Added User Entity Class and associated methods.
    Added User Entity Class and associated methods. Added JUnit tests for
    User class as well as Index Controller Class.

---
+++ ActorNetwork/src/test/java/com/actornetwork/domain/UserTest.java
@@ -0,0 +1,37 @@
+package com.actornetwork.domain;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+	
+	User user; 
+	
+	@BeforeEach
+	void setUp() {
+		user = new User("First", "Last", "test@test.com");
+	}
+		
+	@Test
+	void testFirstName() {
+		assertTrue(user.getFirstName() == "First");
+		user.setFirstName("Test");
+		assertTrue(user.getFirstName() == "Test");
+	}
+	
+	@Test
+	void testLastName() {
+		assertTrue(user.getLastName() == "Last");
+		user.setLastName("Test");
+		assertTrue(user.getLastName() == "Test");
+	}
+	
+	@Test
+	void testEmail() {
+		assertTrue(user.getEmail() == "test@test.com");
+		user.setLastName("Test");
+		assertTrue(user.getLastName() == "Test");
+	}
+}
